6bdc8e8879749c24f0978ddafc3f46505a0982b5
JayaKrishna <namburu1995@gmail.com>
2019-03-19 10:25:43 +0530

    Made minor changes

--- src/App.js
+++ src/App.js
@@ -10,27 +10,6 @@ import travese from '@babel/traverse';
 import generate from 'babel-generator';
 
 class App extends Component {
-
-  generator = (validJSON) => {
-    const reactGenerator = createReactComponentGenerator({ variation: 'CSSModules'});
-    reactGenerator.generateComponent(validJSON)
-        .then(result => {
-          const { code, externalCSS } = result;
-          const ast = babylon.parse(code, { sourceType: 'module', plugins: ["jsx"] });
-          travese(ast, {
-            ReturnStatement(path) {
-              const generatedCode = generate(path.node.argument, {
-                retainLines: false,
-                compact: "auto",
-                concise: false
-              }, code);
-              const el = document.getElementById('holder');
-              el.innerHTML = generatedCode.code;
-            }
-          });
-        });
-  }
-
   editorDidMount(editor, monaco) {
     editor.focus();
   }
@@ -40,7 +19,23 @@ class App extends Component {
     if (flag) {
       try {
         const validJSON = JSON.parse(newValue);
-        this.generator(validJSON);
+        const reactGenerator = createReactComponentGenerator({ variation: 'CSSModules'});
+        reactGenerator.generateComponent(validJSON)
+          .then(result => {
+            const { code, externalCSS } = result;
+            const ast = babylon.parse(code, { sourceType: 'module', plugins: ["jsx"] });
+            travese(ast, {
+              ReturnStatement(path) {
+                const generatedCode = generate(path.node.argument, {
+                  retainLines: false,
+                  compact: "auto",
+                  concise: false
+                }, code);
+                const el = document.getElementById('holder');
+                el.innerHTML = generatedCode.code;
+              }
+            });
+          });
       } catch(e) {
        // console.log(e);
       }
@@ -59,10 +54,10 @@ class App extends Component {
         <div>
           <MonacoEditor
             width="800"
-            height="600"
+            height="100%"
             language="javascript"
             theme="vs-dark"
-            value={JSON.stringify(sample)}
+            value=''
             options={options}
             onChange={this.onChange}
             editorDidMount={this.editorDidMount}
@@ -70,6 +65,14 @@ class App extends Component {
         </div>
         <div id="holder" className="holderWrapper">
           Live Preview of the generated code
+          <p>
+            Link for a collection of valid UIDL
+            <a
+              target="_blank"
+              href="https://github.com/JayaKrishnaNamburu/teleport-code-generators/tree/master/examples/uidl-samples">
+              examples
+            </a>
+          </p>
         </div>
       </div>
     );
