935cffbffc3fa64e024d80140782305f5a668171
JayaKrishna <namburu1995@gmail.com>
2019-03-19 09:51:06 +0530

    Added all dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { sample } from './uidl-sample';
+import MonacoEditor from 'react-monaco-editor';
+import {
+  UIDLValidators,
+  createReactComponentGenerator
+} from '@teleporthq/teleport-code-generators';
+import * as babylon from 'babylon';
+import travese from '@babel/traverse';
+import generate from 'babel-generator';
 
 class App extends Component {
+
+  generator = (validJSON) => {
+    const reactGenerator = createReactComponentGenerator({ variation: 'CSSModules'});
+    reactGenerator.generateComponent(validJSON)
+        .then(result => {
+          const { code, externalCSS } = result;
+          const ast = babylon.parse(code, { sourceType: 'module', plugins: ["jsx"] });
+          travese(ast, {
+            ReturnStatement(path) {
+              const generatedCode = generate(path.node.argument, {
+                retainLines: false,
+                compact: "auto",
+                concise: false
+              }, code);
+              const el = document.getElementById('holder');
+              el.innerHTML = generatedCode.code;
+            }
+          });
+        });
+  }
+
+  editorDidMount(editor, monaco) {
+    editor.focus();
+  }
+
+  onChange(newValue, e) {
+    let flag = UIDLValidators.validateComponent(newValue);
+    if (flag) {
+      try {
+        const validJSON = JSON.parse(newValue);
+        this.generator(validJSON);
+      } catch(e) {
+       // console.log(e);
+      }
+    } else {
+      const el = document.getElementById('holder');
+      el.innerHTML = 'Input a valid UIDL';
+    }
+  }
+
   render() {
+    const options = {
+      selectOnLineNumbers: true
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <div>
+          <MonacoEditor
+            width="800"
+            height="600"
+            language="javascript"
+            theme="vs-dark"
+            value={JSON.stringify(sample)}
+            options={options}
+            onChange={this.onChange}
+            editorDidMount={this.editorDidMount}
+          />
+        </div>
+        <div id="holder" className="holderWrapper">
+          Live Preview of the generated code
+        </div>
       </div>
     );
   }
---
+++ src/uidl-sample.js
@@ -0,0 +1,88 @@
+export const sample = {
+    "$schema": "https://raw.githubusercontent.com/teleporthq/teleport-code-generators/master/src/uidl-definitions/schemas/component.json",
+    "name": "AuthorCard",
+    "propDefinitions": {
+      "authorName": {
+        "type": "string",
+        "defaultValue": "TeleportHQ Rocks"
+      },
+      "avatarUrl": {
+        "type": "string",
+        "defaultValue": "https://picsum.photos/150/150"
+      },
+      "direction": {
+        "type": "string",
+        "defaultValue": "row"
+      }
+    },
+    "content": {
+      "type": "container",
+      "name": "container",
+      "style": {
+        "width": "100%",
+        "height": "100%",
+        "display": "flex",
+        "flexDirection": "left"
+      },
+      "children": [
+        {
+          "type": "container",
+          "name": "image-container",
+          "style": {
+            "backgroundColor": "#333",
+            "borderRadius": "50%",
+            "display": "block",
+            "width": 150,
+            "height": 150,
+            "overflow": "hidden",
+            "border": "none"
+          },
+          "children": [
+            {
+              "type": "image",
+              "name": "img",
+              "attrs": {
+                "src": "https://via.placeholder.com/150",
+                "alt": "Jaya Krishna"
+              }
+            }
+          ]
+        },
+        {
+          "type": "container",
+          "name": "details-container",
+          "style": {
+            "flexDirection": "column",
+            "flex": 1,
+            "marginLeft": "20px"
+          },
+          "children": [
+            {
+              "type": "h3",
+              "name": "autor-name",
+              "style": {
+                "backgroundColor": "#333",
+                "borderRadius": "50%",
+                "display": "block",
+                "width": 150,
+                "height": 150,
+                "overflow": "hidden",
+                "border": "none"
+              },
+              "children": ["Jaya Krishna", "Another child"]
+            },
+            {
+              "type": "container",
+              "name": "autor-desription",
+              "children": ["Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla laoreet metus a nulla rhoncus, et aliquet turpis lacinia."]
+            },
+            {
+                "type": "input",
+                "name": "css-class"
+            }
+          ]
+        }
+      ]
+    }
+  }
+  
